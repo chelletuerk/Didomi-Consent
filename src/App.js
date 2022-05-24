9dc8498c0a454ec814501b3fd4cfcb40a509f9ab
chelle tuerk <tuerkbot@talkingRobot.local>
2022-05-23 18:02:58 -0700

    remove un-used var

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from './Button'
-import { DidomiSDK } from '@didomi/react';
 
 class App extends Component {
   constructor() {
